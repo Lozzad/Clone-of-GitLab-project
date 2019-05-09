70d1f4f38f5b631295fd9233cf77ed5d80059a8c
Nick Slack <nicolas.slack@gmail.com>
2019-05-09 11:06:32 +0100

    Added listeners for the carousel items

---
+++ scripts/carousel-listeners.js
@@ -0,0 +1,34 @@
+var gourd = document.querySelector("#gourd");
+gourd.addEventListener("click", gourdClicked);
+
+function gourdClicked() {
+    console.log("Clicked the gourd");
+}
+
+var headrest = document.querySelector("#headrest");
+headrest.addEventListener("click", gheadrestClicked);
+
+function headrestClicked() {
+    console.log("Clicked the headrest");
+}
+
+var incenseBurner = document.querySelector("#incense-burner");
+incenseBurner.addEventListener("click", incenseBurnerClicked);
+
+function incenseBurnerClicked() {
+    console.log("Clicked the incense-burner");
+}
+
+var toyLion = document.querySelector("#toy-lion");
+toyLion.addEventListener("click", toyLionClicked);
+
+function toyLionClicked() {
+    console.log("Clicked the toy-lion");
+}
+
+var wovenBowl = document.querySelector("#woven-bowl");
+wovenBowl.addEventListener("click", wovenBowlClicked);
+
+function wovenBowlClicked() {
+    console.log("Clicked the woven-bowl");
+}
