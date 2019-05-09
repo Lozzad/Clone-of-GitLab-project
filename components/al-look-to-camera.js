01d335cbe91431f14224d8cd316328c16b92f7d4
Nick Slack <nicolas.slack@gmail.com>
2019-05-09 10:26:45 +0100

    Added look - to - camera

---
+++ components/al-look-to-camera.js
@@ -0,0 +1,19 @@
+AFRAME.registerComponent("al-look-to-camera", {
+    schema: {},
+
+    init() {
+      this.tickFunction = AFRAME.utils.throttle(
+        this.tickFunction,
+        Constants.minFrameMS,
+        this
+      );
+    },
+
+    tickFunction() {
+      this.el.object3D.lookAt(this.el.sceneEl.camera.position);
+    },
+
+    tick() {
+      this.tickFunction();
+    }
+});
