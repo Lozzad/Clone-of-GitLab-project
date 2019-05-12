bac8760a5b2a8d45f286a896204dabef6cddb5c2
Edward Silverton <e.silverton@gmail.com>
2019-05-12 19:58:58 +0100

    moving to webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+
+function resolvePath(p) {
+    return path.resolve(__dirname, p)
+}
+
+const config = {
+    // These are the entry point of our library. We tell webpack to use the name we assign later, when creating the bundle.
+    // We also use the name to filter the second entry point for applying code minification via UglifyJS
+    entry: {
+        'app': ['./src/index.ts']
+    },
+    // The output defines how and where we want the bundles. The special value `[name]` in `filename` tells Webpack to use the name we defined above.
+    // We target a UMD and name it app. When including the bundle in the browser it will be accessible at `window.app`
+    output: {
+        path: resolvePath('build'),
+        filename: '[name].js',
+        libraryTarget: 'umd',
+        library: 'app',
+        umdNamedDefine: true
+    },
+    // Add resolve for `tsx` and `ts` files, otherwise Webpack would
+    // only look for common JavaScript file extension (.js)
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    // Activate source maps for the bundles in order to preserve the original
+    // source when the user debugs the application
+    //devtool: 'source-map',
+    optimization: {
+        minimize: true
+    },
+    // Webpack doesn't understand TypeScript files and a loader is needed.
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    { loader: 'awesome-typescript-loader' }
+                ]
+            }
+        ]
+    }
+}
+
+module.exports = config;
