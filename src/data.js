9338a763f62185bce4bba9896b63e7af37c1b907
Aurelie <bracco.aurelie@gmail.com>
2019-07-08 10:15:27 +0100

    add objects

--- src/data.js
+++ src/data.js
@@ -1,322 +1,322 @@
 let houses = [
-        { 
-        "id": "af", 
-        "posX": -3.5, "posY": 0, "posZ": 2.5, 
-        "rotX": 0, "rotY": 0, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+        {
+        "id": "af",
+        "posX": -3.5, "posY": 0, "posZ": 2.5,
+        "rotX": 0, "rotY": 0, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "af0"}
         ]
     },
-    { 
-        "id": "af2", 
-        "posX": -2, "posY": 0, "posZ": 2, 
-        "rotX": 0, "rotY": -90, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": false, 
+    {
+        "id": "af2",
+        "posX": -2, "posY": 0, "posZ": 2,
+        "rotX": 0, "rotY": -90, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": false,
         "childIds": []
     },
-    { 
-        "id": "ag", 
-        "posX": -0.5, "posY": 0, "posZ": 3.5, 
-        "rotX": 0, "rotY": 90, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "ag",
+        "posX": -0.5, "posY": 0, "posZ": 3.5,
+        "rotX": 0, "rotY": 90, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "ag0"},
             {"id": "ag1"}
         ]
     },
-    { 
-        "id": "ah", 
-        "posX": 2.5, "posY": 0, "posZ": 1, 
-        "rotX": 0, "rotY": 0, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "ah",
+        "posX": 2.5, "posY": 0, "posZ": 1,
+        "rotX": 0, "rotY": 0, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "ah0"}
         ]
     },
-    { 
-        "id": "alice", 
-        "posX": -0.5, "posY": 0, "posZ": 3, 
-        "rotX": 0, "rotY": -90, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": false, 
+    {
+        "id": "alice",
+        "posX": -0.5, "posY": 0, "posZ": 3,
+        "rotX": 0, "rotY": -90, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": false,
         "childIds": []
     },
-    { 
-        "id": "aw", 
-        "posX": 4.5, "posY": 0, "posZ": 5.5, 
-        "rotX": 0, "rotY": 180, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "aw",
+        "posX": 4.5, "posY": 0, "posZ": 5.5,
+        "rotX": 0, "rotY": 180, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "aw0"}
         ]
     },
-    { 
-        "id": "bg", 
-        "posX": 4.5, "posY": 0, "posZ": -1, 
-        "rotX": 0, "rotY": -120, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "bg",
+        "posX": 4.5, "posY": 0, "posZ": -1,
+        "rotX": 0, "rotY": -120, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "bg0"}
         ]
     },
-    { 
-        "id": "cha", 
-        "posX": -4, "posY": 0, "posZ": 5, 
-        "rotX": 0, "rotY": -90, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "cha",
+        "posX": -4, "posY": 0, "posZ": 5,
+        "rotX": 0, "rotY": -90, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "cha0"}
         ]
     },
-    { 
-        "id": "ct", 
-        "posX": -1.5, "posY": 0, "posZ": 3, 
-        "rotX": 0, "rotY": -90, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "ct",
+        "posX": -1.5, "posY": 0, "posZ": 3,
+        "rotX": 0, "rotY": -90, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "ct0"},
             {"id": "ct1"}
         ]
     },
-    { 
-        "id": "dls", 
-        "posX": -1, "posY": 0, "posZ": 2.5, 
-        "rotX": 0, "rotY": 0, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "dls",
+        "posX": -1, "posY": 0, "posZ": 2.5,
+        "rotX": 0, "rotY": 0, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "dls0"},
             {"id": "dls1"}
         ]
     },
-    { 
-        "id": "eb", 
-        "posX": -0.5, "posY": 0, "posZ": 0.5, 
-        "rotX": 0, "rotY": 0, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "eb",
+        "posX": -0.5, "posY": 0, "posZ": 0.5,
+        "rotX": 0, "rotY": 0, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "eb0"},
             {"id": "eb1"}
         ]
     },
-    { 
-        "id": "ef", 
-        "posX": -3.5, "posY": 0, "posZ": 4, 
-        "rotX": 0, "rotY": -90, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "ef",
+        "posX": -3.5, "posY": 0, "posZ": 4,
+        "rotX": 0, "rotY": -90, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "ef0"}
         ]
     },
-    { 
-        "id": "fc", 
-        "posX": 3.5, "posY": 0, "posZ": 0, 
-        "rotX": 0, "rotY": 180, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "fc",
+        "posX": 3.5, "posY": 0, "posZ": 0,
+        "rotX": 0, "rotY": 180, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "fc0"}
         ]
     },
-    { 
-        "id": "fh", 
-        "posX": 0.5, "posY": 0, "posZ": 4, 
-        "rotX": 0, "rotY": 0, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "fh",
+        "posX": 0.5, "posY": 0, "posZ": 4,
+        "rotX": 0, "rotY": 0, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "fh0"}
         ]
     },
-    { 
-        "id": "fs", 
-        "posX": 0.5, "posY": 0, "posZ": 4.5, 
-        "rotX": 0, "rotY": 0, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "fs",
+        "posX": 0.5, "posY": 0, "posZ": 4.5,
+        "rotX": 0, "rotY": 0, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "fs0"}
         ]
     },
-    { 
-        "id": "ga", 
-        "posX": -2, "posY": 0, "posZ": -1, 
-        "rotX": 0, "rotY": -90, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "ga",
+        "posX": -2, "posY": 0, "posZ": -1,
+        "rotX": 0, "rotY": -90, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "ga0"},
             {"id": "ga1"},
             {"id": "ga2"}
         ]
     },
-    { 
-        "id": "jd", 
-        "posX": 4, "posY": 0, "posZ": 5, 
-        "rotX": 0, "rotY": 180, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "jd",
+        "posX": 4, "posY": 0, "posZ": 5,
+        "rotX": 0, "rotY": 180, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "jd0"}
         ]
     },
-    { 
-        "id": "lb", 
-        "posX": -3, "posY": 0, "posZ": 3.5, 
-        "rotX": 0, "rotY": -90, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "lb",
+        "posX": -3, "posY": 0, "posZ": 3.5,
+        "rotX": 0, "rotY": -90, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "lb0"}
         ]
     },
-    { 
-        "id": "lk", 
-        "posX": 1, "posY": 0, "posZ":3.5 , 
-        "rotX": 0, "rotY": -90, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "lk",
+        "posX": 1, "posY": 0, "posZ":3.5 ,
+        "rotX": 0, "rotY": -90, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "lk0"}
         ]
     },
-    { 
-        "id": "lp", 
-        "posX": 1, "posY": 0, "posZ": 3, 
-        "rotX": 0, "rotY": 180, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "lp",
+        "posX": 1, "posY": 0, "posZ": 3,
+        "rotX": 0, "rotY": 180, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "lp0"},
             {"id": "lp1"}
         ]
     },
-    { 
-        "id": "ls", 
-        "posX": 1.5, "posY": 0, "posZ": 2.5, 
-        "rotX": 0, "rotY": 90, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "ls",
+        "posX": 1.5, "posY": 0, "posZ": 2.5,
+        "rotX": 0, "rotY": 90, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "ls0"},
             {"id": "ls1"}
         ]
     },
-    { 
-        "id": "lt", 
-        "posX": 3.5, "posY": 0, "posZ": 4.5, 
-        "rotX": 0, "rotY": 180, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "lt",
+        "posX": 3.5, "posY": 0, "posZ": 4.5,
+        "rotX": 0, "rotY": 180, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "lt0"}
         ]
     },
-    { 
-        "id": "ma", 
-        "posX": 4, "posY": 0, "posZ": 1, 
-        "rotX": 0, "rotY": 180, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "ma",
+        "posX": 4, "posY": 0, "posZ": 1,
+        "rotX": 0, "rotY": 180, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "ma0"}
         ]
     },
-    { 
-        "id": "md", 
-        "posX": 2.5, "posY": 0, "posZ": 0, 
-        "rotX": 0, "rotY": -90, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "md",
+        "posX": 2.5, "posY": 0, "posZ": 0,
+        "rotX": 0, "rotY": -90, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "md0"},
             {"id": "md1"}
         ]
     },
-    { 
-        "id": "nl", 
-        "posX": 2, "posY": 0, "posZ": 0, 
-        "rotX": 0, "rotY": -90, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "nl",
+        "posX": 2, "posY": 0, "posZ": 0,
+        "rotX": 0, "rotY": -90, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "nl0"}
         ]
     },
-    { 
-        "id": "ollie", 
-        "posX": -4, "posY": 0, "posZ": 2, 
-        "rotX": 0, "rotY": 0, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "ollie",
+        "posX": -4, "posY": 0, "posZ": 2,
+        "rotX": 0, "rotY": 0, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "ollie0"},
             {"id": "ollie1"}
         ]
     },
-    { 
-        "id": "pb", 
-        "posX": -3, "posY": 0, "posZ": -1, 
-        "rotX": 0, "rotY": -90, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": false, 
+    {
+        "id": "pb",
+        "posX": -3, "posY": 0, "posZ": -1,
+        "rotX": 0, "rotY": -90, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": false,
         "childIds": []
     },
-    { 
-        "id": "rc", 
-        "posX": -2, "posY": 0, "posZ": 1.5, 
-        "rotX": 0, "rotY": 90, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": false, 
+    {
+        "id": "rc",
+        "posX": -2, "posY": 0, "posZ": 1.5,
+        "rotX": 0, "rotY": 90, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": false,
         "childIds": []
     },
-    { 
-        "id": "rmj", 
-        "posX": -2.5, "posY": 0, "posZ": 2, 
-        "rotX": 0, "rotY": 0, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "rmj",
+        "posX": -2.5, "posY": 0, "posZ": 2,
+        "rotX": 0, "rotY": 0, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "rmj0"}
         ]
     },
-    { 
-        "id": "rw", 
-        "posX": 2, "posY": 0, "posZ": 3, 
-        "rotX": 0, "rotY": -90, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "rw",
+        "posX": 2, "posY": 0, "posZ": 3,
+        "rotX": 0, "rotY": -90, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "rw0"}
         ]
     },
-    { 
-        "id": "ss", 
-        "posX": -1, "posY": 0, "posZ": 4, 
-        "rotX": 0, "rotY": 0, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "ss",
+        "posX": -1, "posY": 0, "posZ": 4,
+        "rotX": 0, "rotY": 0, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "ss0"}
         ]
     },
-    { 
-        "id": "za", 
-        "posX": 3, "posY": 0, "posZ": 4, 
-        "rotX": 0, "rotY": 180, "rotZ": 0, 
-        "scale": 0.05, 
-        "collidable": true, 
+    {
+        "id": "za",
+        "posX": 3, "posY": 0, "posZ": 4,
+        "rotX": 0, "rotY": 180, "rotZ": 0,
+        "scale": 0.05,
+        "collidable": true,
         "childIds": [
             {"id": "za0"},
             {"id": "za1"}
@@ -324,7 +324,25 @@ let houses = [
     }
 ]
 
-let objects = []
+//les identifiants de childIDs ne devraient pas être ceux des objets ?
+let objects = [
+  {
+    "id": "letter",
+    "scale": 1.2
+  },
+  {
+    "id": "cat-walking",
+    "scale": 1.2
+  }
+  {
+    "id": "cat-sitting",
+    "scale": 1.2
+  }
+  {
+    "id": "signpost",
+    "scale": 3
+  }
+]
 
 function addHouses() {
     houses.forEach(houseData => {
@@ -340,13 +358,13 @@ function addHouses() {
         );
         house.object3D.scale.set(houseData.scale, houseData.scale, houseData.scale);
         house.setAttribute('gltf-model', '/assets/' + houseData.id + '.gltf');
-        if (houseData.collidable) { 
+        if (houseData.collidable) {
             house.setAttribute('class', 'collidable');
             house.setAttribute('animation-mixer', {clip: 'closed'});
             house.setAttribute('box', {carouselId: 'carousel'});
         }
         marker.appendChild(house);
-        
+
     });
 }
 
