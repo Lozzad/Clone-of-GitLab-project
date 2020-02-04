ce891e0443324ef7c19f05b3d0ad65160e090bbb
ld469 <laurie.dibble1@gmail.com>
2020-02-04 16:03:36 +0000

    housebuilder uses schemas

--- src/components/houseBuilder.ts
+++ src/components/houseBuilder.ts
@@ -1,20 +1,19 @@
 import { BaseComponent } from "../BaseComponent";
 
 interface HouseBuilderComponent extends BaseComponent {
-    houseData: [] | null;
+
 }
 
 export default AFRAME.registerComponent("housebuilder", {
     schema: {
         //property: {type: '', default: x}
-        houseData: { type: 'any', default: null }
+        houseData: { default: [] }
     },
-    houseData: null,
     //multiple: false,
 
     init: function () {
         console.log("Housebuilder init");
-        console.log(this.houseData);
+        console.log(this.data.houseData);
     },
 
     addEventListeners() { },
