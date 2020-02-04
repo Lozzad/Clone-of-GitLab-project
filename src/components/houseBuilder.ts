6ef408ada7cfaff3f5331db4e536191cfa6613a8
ld469 <laurie.dibble1@gmail.com>
2020-02-04 15:47:53 +0000

    log diff

--- src/components/houseBuilder.ts
+++ src/components/houseBuilder.ts
@@ -14,7 +14,7 @@ export default AFRAME.registerComponent("housebuilder", {
 
     init: function () {
         console.log("Housebuilder init");
-        console.log(this.houseData);
+        console.log(this);
     },
 
     addEventListeners() { },
